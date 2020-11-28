2c3575007163ce8e47fe56d33105d05f3c992576
surajsinha2512 <61259615+surajsinha2512@users.noreply.github.com>
2020-11-29 00:21:37 +0530

    push

---
+++ miniProjects/tictactoe/tryHere/valid.js
@@ -0,0 +1,57 @@
+
+const p1= document.getElementById('p1');
+const p2= document.getElementById('p2');
+const subAuth= document.getElementById('sub');
+
+var p1flag=false;
+var p2flag=false;
+
+p1.addEventListener("input", function(a){
+    var p1Auth=a.target.value;
+    localStorage.setItem("xname",p1Auth);
+    // console.log(p1Auth);
+    if(p1Auth.length!=0)
+    {
+        p1flag=true;
+    }
+    else{
+        p1flag=false;
+    }
+    checkSubmit();
+});
+
+p2.addEventListener("input", function(a){
+    var p2Auth=a.target.value;
+    localStorage.setItem("oname",p2Auth);
+    if(p2Auth.length!=0)
+    {
+        p2flag=true;
+    }
+    else{
+        p2flag=false;
+    }
+    checkSubmit();
+});
+
+function checkSubmit()
+{
+    if((p1flag && p2flag))
+    {
+        subAuth.className="btn btn-success";
+        subAuth.disabled=false;
+        subAuth.addEventListener("click",(e)=>{
+            e.preventDefault();
+            location.href="./startGame/"
+        })
+    }
+    else{
+        subAuth.disabled=true;
+        
+    }
+}
+
+function myfunction(){
+
+
+    location.href='./startGame/';
+}
